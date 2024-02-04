474462c6fbd70580f0d89bebdc70a0e18e673c3d
Sanket-Borade <sanketborade00@gmail.com>
2024-02-04 13:31:22 +0530

    first

---
+++ src/com/addressbook/ConnectionFactory.java
@@ -0,0 +1,43 @@
+package com.addressbook;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+//Class to retrieve connection for database and login verfication.
+public class ConnectionFactory {
+
+    static final String driver = "com.mysql.cj.jdbc.Driver";
+    static final String url = "jdbc:mysql://localhost:3306/addressbook";
+    static String username = "root";
+    static String password = "Sanket@1433";
+
+    Properties prop;
+
+    Connection conn = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+
+    public ConnectionFactory(){
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            statement = conn.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConn() {
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
